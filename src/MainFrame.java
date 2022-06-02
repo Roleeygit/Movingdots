a81d518b92c9e10100ea4d68750b79645ad3918b
Roleeygit <juhaszroli2001@gmail.com>
2022-06-02 11:53:41 +0200

    refakt for ciklusba

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -15,47 +15,21 @@ public class MainFrame extends JFrame {
 
 
     }
+
+
 class SajátComponent extends JComponent {
     @Override
     public void paint(Graphics g) {
 
-        int x = (int) Math.floor(Math.random()*297+2);
-        int x1 = (int) Math.floor(Math.random()*297+2);
-        int x2 = (int) Math.floor(Math.random()*297+2);
-        int x3 = (int) Math.floor(Math.random()*297+2);
 
-        int y = (int) Math.floor(Math.random()*297+2);
-        int y1 = (int) Math.floor(Math.random()*297+2);
-        int y2 = (int) Math.floor(Math.random()*297+2);
-        int y3 = (int) Math.floor(Math.random()*297+2);
-
-        System.out.println(x);
-        System.out.println(y);
-
-        // g.setColor(Color.black);
-        // g.fillOval(5, 30, 10, 10);
-        // g.setColor(Color.black);
-        // g.fillOval(5, 15, 10, 10);
-        // g.setColor(Color.black);
-        // g.fillOval(20, 10, 10, 10);
-        // g.setColor(Color.black);
-        // g.fillOval(40, 15, 10, 10);
-
-        g.setColor(Color.black);
-        g.fillOval(x, y, 10, 10);
-
-        g.setColor(Color.black);
-        g.fillOval(x1, y1, 10, 10);
-
-        g.setColor(Color.black);
-        g.fillOval(x2, y2, 10, 10);
-
-        g.setColor(Color.black);
-        g.fillOval(x3, y3, 10, 10);
+        for(int db=1;db<=4;db++){
+            int x=(int)Math.floor(Math.random()*287+2);
+            int y=(int)Math.floor(Math.random()*267+30);
+            g.fillOval(x, y, 10,10);
 
         // g.setColor(Color.black);
         // g.fillRect(x, y, 50, 50);
-
+        }
         
     }
 
